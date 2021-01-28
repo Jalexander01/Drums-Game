ce157771cead5a39ef975105b372a8cf2995bc67
Jalexander01 <60295314+Jalexander01@users.noreply.github.com>
2021-01-28 13:22:09 -0600

    added functionality

---
+++ JS/index.js
@@ -0,0 +1,12 @@
+
+var numberOfButton = document.querySelectorAll(".drum").length;
+
+
+for (var i = 0; i < numberOfButton; i++){
+  document.querySelectorAll("button")[i].addEventListener("click", function(){
+
+      alert("hello");
+  });
+
+
+}
