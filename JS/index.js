f26189f720507f492498c5640fa5ce6589ea07aa
Jalexander01 <60295314+Jalexander01@users.noreply.github.com>
2021-01-28 18:25:09 -0600

    send

--- JS/index.js
+++ JS/index.js
@@ -4,8 +4,9 @@ var numberOfButton = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfButton; i++){
   document.querySelectorAll("button")[i].addEventListener("click", function(){
+  var audio = new Audio("sounds/crash.mp3");
+  audio.play();
 
-      alert("hello");
   });
 
 
